5a7c75e4379450fb1fd82b4e234b2cd4d12ee100
fuseina <fuseina@gmail.com>
2018-04-04 10:11:23 -0500

    database

--- app/config/connection.js
+++ app/config/connection.js
@@ -8,7 +8,7 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "We.R0ck!",
+    password: "",
     database: "devtech_db"
   });
 }

8c3c7c8a58a28963e373be71486de50c7f2eaf29
fuseina <fuseina@gmail.com>
2018-04-04 11:43:42 -0500

    topicName typo

--- app/models/topics.js
+++ app/models/topics.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Topics = sequelize.define("Topics",
   {
-    tipicName: {
+    topicName: {
       type: DataTypes.STRING,
       allowNull: false
     },

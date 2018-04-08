5c69ddf3d01fdd13f6191264985179bd9cdb5ad1
Whitney Welch <whitneywelch@whitneys-mbp.attlocal.net>
2018-04-08 11:30:11 -0500

    Login/signup work

--- app/client/src/Signup/Signup.js
+++ app/client/src/Signup/Signup.js
@@ -1,37 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Signup.css';
+import API from "../utils/API";
 
-const Signup = () => {
-    
+class Signup extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = [{firstName: '', lastName:'', email:'', email2:'', psw:'', psw2:''}];
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+          [name]: value
+        });
+      };
+
+    handleSubmit(event) {
+        alert(`Your signup was successful ${this.state.firstName}!`);
+        event.preventDefault();
+        API.addNewUser({firstName:this.state.firstName, lastName:this.state.lastName, email:this.state.email, password:this.state.psw})
+            .catch(err => console.log(err));
+    }
+
+    render() {
         return (
             <form className='signupBox' onSubmit={this.handleSubmit}>
                 <label>First Name:</label>
-                    <input type='text' name='fnSignup' value="" placeholder='First Name' />
+                    <input type='text' name='firstName' value={this.state.firstName} onChange={this.handleInputChange} placeholder='First Name' />
                 <br />
                 <label>Last Name:</label>
-                    <input type='text' name='lnSignup' value="" placeholder='Last Name' />
+                    <input type='text' name='lastName' value={this.state.lastName} onChange={this.handleInputChange} placeholder='Last Name' />
                 <br />
                 <label>Email Address:</label>
-                    <input type='text' name='signupEmail' value="" placeholder='Email Address' />
+                    <input type='text' name='email' value={this.state.email} onChange={this.handleInputChange} placeholder='Email Address' />
                 <br />
                 <label>Re-Enter Email Address:</label>
-                    <input type='text' name='signupEmail2' value="" placeholder='Re-Enter Email Address' />
+                    <input type='text' name='email2' value={this.state.email2} onChange={this.handleInputChange} placeholder='Re-Enter Email Address' />
                 <br />
                 <label>Password:</label>
-                    <input type='password' name='signupPass' value="" placeholder='Password' />
+                    <input type='password' name='psw' value={this.state.psw} onChange={this.handleInputChange} placeholder='Password' />
                 <br />
                 <label>Re-Enter Password:</label>
-                    <input type='text' name='signupPass2' value="" placeholder='Re-Enter Password' />
-                <br />
-                <label>Zip Code:</label>
-                    <input type='text' name='SignupZip' value="" placeholder='Zip Code' />
-                <br />
-                <label>Date of Birth:</label>
-                    <input type='text' name='signupDOB' value="" placeholder='Date of Birth' />
+                    <input type='text' name='psw2' value={this.state.psw2} onChange={this.handleInputChange} placeholder='Re-Enter Password' />
                 <br />
                 <input type='submit' value='Submit'/>
             </form>
         )
+    };
 };
-
 export default Signup;

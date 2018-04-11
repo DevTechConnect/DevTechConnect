f8b12b6e061ea3f8d03550492c9be8626ddc3a03
Whitney Welch <whitneywelch@whitneys-mbp.attlocal.net>
2018-04-10 23:13:27 -0500

    Components created for limitedFModule. Routing for tabs created. JSX and content inserted for LimitedFModule.

--- app/client/src/LimitedFModule/LimitedFModule.js
+++ app/client/src/LimitedFModule/LimitedFModule.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Steps from './Steps/Steps';
 
 import './LimitedFModule.css';
 
 const LimitedFModule = () => {
     return (
         <div>
-            <Navbar />
-            <p>Limited Focus Test</p>
+            <h2>Welcome to your {props.selTrackName} learning track.</h2>
+            <p>Follow along and check off each step you complete to track of your progress. Most importantly, don't skip the practice; practice will be your quickest teacher in code. We'll give you achievements along the way - you can find those on your <a href='#'>Member Page</a>. 
+            <p>We're' happy you're here. Stick around, and like always, keep moving.</p>
+            <div>
+                <Steps />
+                <Route exact path="/Step1" component={Step1} />
+                <Route exact path="/Step2" component={Step1} />
+                <Route exact path="/Step3" component={Step3} />
+                <Route exact path="/Step4" component={Step4} />
+                <Route exact path="/Practice" component={Step4} />
+                <Route exact path="/NextTrack" component={Step4} />
+            </div>
         </div>
     )
 };
--- app/client/src/MemberP/MemberP.js
+++ app/client/src/MemberP/MemberP.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import NavbarNLI from '../Navbar/Navbar';
+import Sidebar from './Sidebar/Sidebar';
+import Achievement from './Achievement/Achievement';
 
 import './MemberP.css';
 
-const MemberP = () => {
+class MemberP extends Component {
+    constructor {
+        super(props);
+        this.state = {''};
+    }
     return (
         <div>
             <Navbar />
-            <p>Member Page Test</p>
+            <h2>Welcome back {props.fName}!</h2>
+            <p>Hop back in where you left off.</p>
+            <button type='button' name='pickUpMemTop'>{props.lastTrackName}</button>
+            <Achievement />
+            <Achievement />
+            <Achievement />
         </div>
     )
 };

1e93ac162ff5fb6160199f0bb5e57ed17c63f812
Whitney Welch <whitneywelch@Whitneys-MacBook-Pro.local>
2018-04-17 18:57:33 -0500

    TrackQV added to related tracks.

--- app/client/src/RelTrack/RelTrack.js
+++ app/client/src/RelTrack/RelTrack.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import TrackQV from '../TrackQV/TrackQV';
+
 import './RelTrack.css';
 
 const RelTrack = () => {
     return (
         <div>
-            <h2>Related Track</h2>
+            <h2>Related Tracks</h2>
+            <TrackQV />
+            <TrackQV />
+            <TrackQV />
         </div>
     )
 };

b34e6a477ada5bdad55eadbe5061b8402d50cb03
Whitney Welch <whitneywelch@whitneys-mbp.attlocal.net>
2018-04-17 01:52:43 -0500

    Logo added.

--- app/client/src/Navbar/Navbar.js
+++ app/client/src/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = (props) => {
     return (
         <div>
             <nav>
-                <Image className='inline logo' src={'../../public/assets/images/tempLogo.jpg'} width={150} height={100} mode='fit' onClick={props.homeClickHandler}/>  
+                <Image className='inline logo' src={require('./logo.png')} width={300} height={81.25} mode='fit' onClick={props.homeClickHandler}/>  
                 <ul className='inline'>
                     <li onClick={props.memClickHandler}>Member Page</li>
                     <li onClick={props.trackClickHandler}>Limited Focus Tracks</li>
